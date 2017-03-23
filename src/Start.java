86f100fa0bb77b04ef410dca2c80fda3cea50192
Peter Schriever <peterzen72@gmail.com>
2017-03-23 11:23:30 +0100

    hello world added

---
+++ src/Start.java
@@ -0,0 +1,9 @@
+/**
+ * Created by peterzen on 2017-03-23.
+ * Part of the othello project.
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
